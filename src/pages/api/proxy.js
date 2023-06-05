b4ef5a731d1ed4f76b6da36049f47b608626dbb4
don Pablo <pablo.brin@gmail.com>
2023-06-05 13:59:44 +0200

    Merge remote-tracking branch 'origin/master'

--- src/pages/api/proxy.js
+++ src/pages/api/proxy.js
@@ -6,29 +6,28 @@ const globalJS = require('../../utils/js');
 const Jimp = require('jimp');
 
 const globalSpin = require('../../utils/helpers');
+const origin = window.location.origin;
 
 module.exports = (req, res) => {
-
     createProxyMiddleware({
         changeOrigin: true,
         on: {
             proxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
-
                 const imageTypes = ['image/png', 'image/jpg', 'image/jpeg', 'image/gif'];
 
-                if (imageTypes.includes(proxyRes.headers['content-type'])) {
-                    let image = await Jimp.read(responseBuffer)
-                    image.flip(true, false).sepia().pixelate(5)
-                    return image.getBufferAsync(Jimp.AUTO)
-                } else {
-                    return replaceFunc(globalReplace, replaceFunc(globalSpin,
-                        replaceFunc(process.env.REPLACE,
-                            responseBuffer.toString('utf8').replaceAll(process.env.TARGET,
-                                this.props.location.origin)))).replace('</head>',
-                        '<script>' + includeFunc(process.env.JS,
-                            includeFunc(globalJS)) + '</script><style>' + includeFunc(process.env.CSS,
-                            includeFunc(globalCSS)) + '</style></head>')
-                }
+                    if (imageTypes.includes(proxyRes.headers['content-type'])) {
+                        let image = await Jimp.read(responseBuffer)
+                        image.flip(true, false).sepia().pixelate(5)
+                        return image.getBufferAsync(Jimp.AUTO)
+                    } else {
+                        return replaceFunc(globalReplace, replaceFunc(globalSpin,
+                            replaceFunc(process.env.REPLACE,
+                                responseBuffer.toString('utf8').replaceAll(process.env.TARGET,
+                                    origin)))).replace('</head>',
+                            '<script>' + includeFunc(process.env.JS,
+                                includeFunc(globalJS)) + '</script><style>' + includeFunc(process.env.CSS,
+                                includeFunc(globalCSS)) + '</style></head>')
+                    }
 
             }),
         },
