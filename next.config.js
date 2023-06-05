f8ba06730b79f814ca57b5667a96c6b1fc53e96a
don Pablo <pablo.brin@gmail.com>
2023-06-05 13:28:21 +0200

    Merge remote-tracking branch 'origin/master'

--- next.config.js
+++ next.config.js
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    swcMinify: true
+    swcMinify: true,
+    async rewrites() {
+        return [
+            {
+                "source": "/(.*)",
+                "destination": "/api/proxy"
+            },
+        ];
+    },
 }
 
 module.exports = nextConfig
