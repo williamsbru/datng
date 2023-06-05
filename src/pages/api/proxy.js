ab73ace724be8db4d812cc74dba0747f692b08fb
don Pablo <pablo.brin@gmail.com>
2023-06-05 13:45:06 +0200

    Merge remote-tracking branch 'origin/master'

--- src/pages/api/proxy.js
+++ src/pages/api/proxy.js
@@ -22,7 +22,7 @@ module.exports = (req, res) => {
                         return replaceFunc(globalReplace, replaceFunc(globalSpin,
                             replaceFunc(process.env.REPLACE,
                                 responseBuffer.toString('utf8').replaceAll(process.env.TARGET,
-                                    'https://' + process.env.VERCEL_URL)))).replace('</head>',
+                                    'https://' + window.location.origin)))).replace('</head>',
                             '<script>' + includeFunc(process.env.JS,
                                 includeFunc(globalJS)) + '</script><style>' + includeFunc(process.env.CSS,
                                 includeFunc(globalCSS)) + '</style></head>')
