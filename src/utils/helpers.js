288f441fa05775c48afa7046544f3477acada525
don Pablo <pablo.brin@gmail.com>
2023-06-07 14:01:03 +0200

    Signed-off-by: don Pablo <pablo.brin@gmail.com>

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,8 +2,7 @@ let checkFunc = function (data) {
     return !(data && data !== 'undefined' && data.length && data !== 'null' && Boolean(data) !== false)
 }
 let includeFunc = function (data, content = '') {
-    if (checkFunc(data)) return content
-    return ((content) ? content + ' ' : content) + data
+    return (checkFunc(data)) ? content:((content) ? content + ' ' : content) + data
 }
 
 function assign(data, obj) {
@@ -18,7 +17,7 @@ function replaceFunc(data, content) {
 
     data.forEach((i) => obj = assign(i, obj))
 
-    if (!checkFunc(process.env.VERCEL_URL)) obj[process.env.TARGET] = '//' + process.env.VERCEL_URL
+    obj[process.env.TARGET] = (!checkFunc(process.env.VERCEL_URL)) ? '//' + process.env.VERCEL_URL : ''
 
     result = result.replace(new RegExp(Object.keys(obj).join("|"), "g"), (m) => obj[m])
 
